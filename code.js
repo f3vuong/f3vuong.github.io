688e54da966897e3c7dca25d2652f57bfd99b319
Francesca Vuong <f3vuong@uwaterloo.ca>
2020-05-24 23:54:45 -0400

    fixed drop down to be button, added buttons to travel page

--- code.js
+++ code.js
@@ -1,13 +1,53 @@
-function readMore() {
-    var nycText = document.getElementById("nyc-text");
-    var btnText = document.getElementById("read-story");
+/*dropdown*/
+function dropDown(){
+  var button = document.getElementById("dropbtn");
+  var content = document.getElementById("dropdown-content");
+
+  if (content.style.display === "block"){
+    content.style.display = "none";
+  }else {
+    content.style.display = "block";
+  }
+
+
+}
+/* travel page */
+
+
+  function readMore() {
+    var text = document.getElementById("nyc-text");
+    var btnText = document.getElementById("nyc-btn");
   
-    if (nycText.style.display === "block") {
-      btnText.innerHTML = "Read more";
-      nycText.style.display = "none";
+    if (text.style.display === "block") {
+      btnText.innerHTML = "Read More";
+      text.style.display = "none";
     } else {
-      btnText.innerHTML = "Read less";
-      nycText.style.display = "block";
+      btnText.innerHTML = "Read Less";
+      text.style.display = "block";
+     
+    }
+
+    var text = document.getElementById("van-text");
+    var btnText = document.getElementById("van-btn");
+
+    if (text.style.display === "block") {
+      btnText.innerHTML = "Read More";
+      text.style.display = "none";
+    } else {
+      btnText.innerHTML = "Read Less";
+      text.style.display = "block";
+     
+    }
+
+    var text = document.getElementById("ice-text");
+    var btnText = document.getElementById("ice-btn");
+
+    if (text.style.display === "block") {
+      btnText.innerHTML = "Read More";
+      text.style.display = "none";
+    } else {
+      btnText.innerHTML = "Read Less";
+      text.style.display = "block";
      
     }
   }
